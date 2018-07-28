92b19ce8b9380ea68bc1675a8046bc20f3f07c16
Jakub Dranczewski <domdlapsa@gmail.com>
2018-07-28 19:02:15 +0200

    Cosmetics on the GraphFrame example

--- matplotlib_tkinter/matplotlib_frame_example.py
+++ matplotlib_tkinter/matplotlib_frame_example.py
@@ -1,6 +1,7 @@
+# An example showing how to use the GraphFrame class
 import tkinter as Tk
 import matplotlib_frame
-from scipy import sin, cos, linspace
+from scipy import sin, cos, tan, linspace, pi
 
 # Create the root window
 root = Tk.Tk()
@@ -10,13 +11,13 @@ gf = matplotlib_frame.GraphFrame(root, show_toolbar=True)
 gf.pack(side=Tk.TOP)
 
 # Plot two functions
-space = linspace(0, 10, 100)
+space = linspace(0, 4*pi, 100)
 gf.subplot.plot(space, cos(space))
 line, = gf.subplot.plot(space, sin(space))
 
 # Add a button for changing data
 def change_data():
-    line.set_data(space, cos(space))
+    line.set_data(space, cos(space+pi/4))
     # Note that the canvas needs to be redrawn after a data change
     gf.draw()
 b = Tk.Button(root, text="Change data", command=change_data)
