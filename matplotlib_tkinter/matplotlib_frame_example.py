e9cd0c179aaf2af85dd5bc5dd0e5142abba189ac
Jakub Dranczewski <domdlapsa@gmail.com>
2018-07-28 18:57:05 +0200

    Added comments and some additional kwargs to GraphFrame Now the default behaviour is to not show the toolbar.

--- matplotlib_tkinter/matplotlib_frame_example.py
+++ matplotlib_tkinter/matplotlib_frame_example.py
@@ -2,22 +2,25 @@ import tkinter as Tk
 import matplotlib_frame
 from scipy import sin, cos, linspace
 
+# Create the root window
 root = Tk.Tk()
 
-gf = matplotlib_frame.GraphFrame(root)
+# Create a GraphFrame and pack it inside of root
+gf = matplotlib_frame.GraphFrame(root, show_toolbar=True)
 gf.pack(side=Tk.TOP)
 
+# Plot two functions
 space = linspace(0, 10, 100)
 gf.subplot.plot(space, cos(space))
 line, = gf.subplot.plot(space, sin(space))
 
-
+# Add a button for changing data
 def change_data():
     line.set_data(space, cos(space))
+    # Note that the canvas needs to be redrawn after a data change
     gf.draw()
-
-
 b = Tk.Button(root, text="Change data", command=change_data)
 b.pack()
 
+# Start the mainloop
 root.mainloop()
